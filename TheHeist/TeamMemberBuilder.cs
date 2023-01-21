1c47b33bf314f7d0abdf4f9013a4d6047338dce1
r-stroud <stroudrobert86@gmail.com>
2023-01-21 11:20:26 -0600

    Added notes and new changes for presentation

--- TheHeist/TeamMemberBuilder.cs
+++ TheHeist/TeamMemberBuilder.cs
@@ -1,4 +1,6 @@
-﻿namespace TheHeist;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace TheHeist;
 
 //the job of this class is to handle collecting info and creating new team members
 
@@ -11,23 +13,141 @@ public class TeamMemberBuilder
 
 
         List<TeamMember> team = new List<TeamMember>() { };
-Console.WriteLine("Plan Your Heist!");
-        Console.WriteLine("Please enter the bank's difficulty level.");
+        string title = "\r\n$$$$$$$$\\ $$\\   $$\\ $$$$$$$$\\       $$\\   $$\\ $$$$$$$$\\ $$$$$$\\  $$$$$$\\ $$$$$$$$\\ \r\n\\__$$  __|$$ |  $$ |$$  _____|      $$ |  $$ |$$  _____|\\_$$  _|$$  __$$\\\\__$$  __|\r\n   $$ |   $$ |  $$ |$$ |            $$ |  $$ |$$ |        $$ |  $$ /  \\__|  $$ |   \r\n   $$ |   $$$$$$$$ |$$$$$\\          $$$$$$$$ |$$$$$\\      $$ |  \\$$$$$$\\    $$ |   \r\n   $$ |   $$  __$$ |$$  __|         $$  __$$ |$$  __|     $$ |   \\____$$\\   $$ |   \r\n   $$ |   $$ |  $$ |$$ |            $$ |  $$ |$$ |        $$ |  $$\\   $$ |  $$ |   \r\n   $$ |   $$ |  $$ |$$$$$$$$\\       $$ |  $$ |$$$$$$$$\\ $$$$$$\\ \\$$$$$$  |  $$ |   \r\n   \\__|   \\__|  \\__|\\________|      \\__|  \\__|\\________|\\______| \\______/   \\__|   \r\n                                                                                   \r\n                                                                                   \r\n                                                                                   \r\n";
+        
+        for (int i = 0; i < 3; i++)
+        {
+            Console.CursorVisible = false;
+Console.Clear();
+            Thread.Sleep(400);
+
+            Console.WriteLine(title);
+            Thread.Sleep(400);
+            
+        }
+        Console.CursorVisible=true;
+        
+
+        Console.ReadKey(true);
+
+        string step1 = "Step 1: Plan your Heist!";
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Blue;
+
+        for (int i = 0; i < step1.Length; i++)
+        {
+            Console.Write(step1[i]);
+            Thread.Sleep(30);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Black;
+
+        string bankEntry = "Please enter the target bank's difficulty level: ";
+        for (int i = 0; i < bankEntry.Length; i++)
+        {
+            Console.Write(bankEntry[i]);
+            Thread.Sleep(30);
+        }
+
         string bankLevel = Console.ReadLine();
         int bankLevelAsInt;
         while(! int.TryParse(bankLevel, out bankLevelAsInt))
         {
-            Console.WriteLine("Please enter an integer.");
+            Console.Write("Please enter an integer: ");
             bankLevel = Console.ReadLine();
         }
         Console.Clear();
 
+        Console.WriteLine(title);
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.BackgroundColor = ConsoleColor.White;
+
+        string targetStatement = $"Target Bank's Difficulty Level: {bankLevelAsInt}";
+
+        for (int i = 0; i < targetStatement.Length; i++)
+        {
+            Console.Write(targetStatement[i]);
+            Thread.Sleep(30);
+        }
+
+        Console.WriteLine() ;
+        Console.WriteLine();
+
+        string luckStatement = "Luck: 10";
+
+        for (int i = 0; i< luckStatement.Length; i++)
+        {
+            Console.Write(luckStatement[i]);
+            Thread.Sleep(30);
+        }
+
+        Console.WriteLine();
+        Console.WriteLine();
+
+        string teamSkillStatement = "Current Team's Skill Level: 0";
+        
+        for (int i = 0; i< teamSkillStatement.Length; i++)
+        {
+            Console.Write(teamSkillStatement[i]);
+            Thread.Sleep(30);
+        }
+
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.BackgroundColor = ConsoleColor.Black;
+
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.WriteLine();
+
         int counter = 0;
+
+Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            string step2 = "Step 2: Create your team!";
+            for (int i = 0; i < step2.Length; i++)
+            {
+                Console.Write(step2[i]);
+                Thread.Sleep(30);
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+
+        Console.WriteLine();
+
+        Console.Clear();
+        
+        int teamSkill = 0;
 
         while (true)
         {
-            Console.WriteLine("Make your team!");
-            
+
+            Console.WriteLine(title);
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine($"Target Bank's Difficulty Level: {bankLevelAsInt} ");
+            Console.WriteLine();
+            Console.WriteLine("Luck: 10");
+            Console.WriteLine();
+            Console.WriteLine($"Current Team's Skill Level: {teamSkill}");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.WriteLine(step2);
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Black;
+
             counter++;
 
             TeamMember teamMember = new TeamMember();
@@ -61,6 +181,8 @@ Team Member {counter}
 
             teamMember.SkillLevel = skillAsInt;
 
+            teamSkill += skillAsInt;
+
             Console.Write("Enter courage factor: ");
 
             string courageInput = Console.ReadLine();
@@ -80,22 +202,40 @@ Team Member {counter}
             Console.Clear();
         }
 
+        Console.WriteLine(title);
 
+        string teamOf0 = "You do not have anyone on your team. Start Recruiting!";
+        string teamOf1 = $"You only have {team.Count} member on your team. You might want to add more team members before starting a job.";
+        string teamOfMany = $"You have {team.Count} members on your team. Good luck!";
 
         if (team.Count < 1)
         {
-            Console.WriteLine($"You do not have anyone on your team. Start Recruiting!");
+            for (int i = 0; i < teamOf0.Length; i++)
+            {
+                Console.Write(teamOf0[i]);
+                Thread.Sleep(30);
+            }
         }
         else if (team.Count == 1)
         {
-            Console.WriteLine($"You only have {team.Count} member on your team. You might want to add more team members before starting a job.");
+            for (int i = 0; i < teamOf1.Length; i++)
+            {
+                Console.Write(teamOf1[i]);
+                Thread.Sleep(30);
+            }
         }
         else if (team.Count > 1)
         {
-            Console.WriteLine($"You have {team.Count} members on your team. Good luck!");
+            for (int i = 0; i < teamOfMany.Length; i++)
+            {
+                Console.Write(teamOfMany[i]);
+                Thread.Sleep(30);
+            }
         }
 
-        Console.WriteLine("Please enter the number of trial runs you would like to perform.");
+        Console.WriteLine();
+        Console.WriteLine();
+        Console.Write("Please enter the number of trial runs you would like to perform: ");
         string numOfTrials = Console.ReadLine();
         int numOfTrialsAsInt;
         while (!int.TryParse(numOfTrials, out numOfTrialsAsInt) || numOfTrialsAsInt <= 0)
@@ -104,11 +244,23 @@ Team Member {counter}
             numOfTrials = Console.ReadLine();
         }
         Console.Clear();
+
+        string loading ="";
+
+        for (int i = 0; i < 101; i++)
+
+        {
+            Console.Clear();
+            loading = $"{i}%";
+            Console.WriteLine($"LOADING RESULTS: {loading}");
+            Thread.Sleep(10);
+        } 
+        
         int groupSkill = 0;
 
         for (int i = 0; i < team.Count; i++)
         {
-            //Console.WriteLine(team[i]);
+
             groupSkill += team[i].SkillLevel;
         }
 
@@ -122,7 +274,6 @@ Team Member {counter}
             int luckValue = randomNum.Next(-10, 10);
             bankSkill += luckValue;
 
-
             Console.Write(@$"
 ====================================
 TRIAL NO. {i+1}
@@ -131,45 +282,58 @@ Bank Skill Level: {bankSkill}
 ");
             if (groupSkill >= bankSkill)
             {
+
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.Write(@"
 The heist was a success!");
                 successCounter++;
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.Write(@"
 The heist has failed! Flee the scene!");
                 failureCounter++;
             }
-
+            Console.ForegroundColor = ConsoleColor.White;
+            Thread.Sleep(200);
         }
 
+        Console.WriteLine();
+        Console.WriteLine("====================================");
+        Console.WriteLine("RESULTS");
+        Console.WriteLine();
         if(successCounter == 1)
         {
+
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write(@$"
-============================================
 The heist succeeded {successCounter} time.");
         }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Green;
             Console.Write(@$"
-============================================
 The heist succeeded {successCounter} times.");
         }
 
+        Console.WriteLine();
+
         if (failureCounter == 1)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.Write(@$"
-The heist failed {failureCounter} time.
-============================================");
+The heist failed {failureCounter} time.");
         }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.Write(@$"
-The heist failed {failureCounter} times.
-===========================================");
+The heist failed {failureCounter} times.");
         }
-
+        Console.ForegroundColor = ConsoleColor.White;
+        Console.WriteLine();
+        Console.WriteLine("====================================");
     }
 
 }
